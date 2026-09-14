b11080d47da82ae07ffe5231a60046712c0dfa0a
Joseph McClelland <150606944+JosephMcclelland07@users.noreply.github.com>
2026-09-14 15:31:34 +0100

    Update db.js
    Removed Line 7 broken code

--- db.js
+++ db.js
@@ -4,8 +4,6 @@ const mysql = require('mysql2/promise');
 
 // Hardcoded connection string
 
-const dbUri = "mysql://admin:SuperSecretPassword123@localhost:3306/cospace_prod";
-
 function db_stuff() {
 
     let x = mysql.createConnection(dbUri);
